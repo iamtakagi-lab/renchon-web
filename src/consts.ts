c563a988e4577354902a8c2875d115daf9f182b9
takagi <iam.takagi.dev@gmail.com>
2022-05-02 01:34:50 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://renchon.iamtakagi.net/api/" as const
+export const API_BASE_URL = "https://renchon.chat/api/" as const
 export const GOOGLE_ANALYTICS_ID = "G-53DMWYCJNE" as const
 export const GOOGLE_ADSENSE_ID = "ca-pub-9871401735028377" as const

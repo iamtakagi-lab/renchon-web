a88fd4ba9989023f182e532f38d2439acf408e3f
takagi <iam.takagi.dev@gmail.com>
2022-10-18 06:11:08 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,14 +74,6 @@ const IndexPage = () => {
           </a>
         </div>
         <a
-          href="https://github.com/iamtakagi"
-          target="_blank"
-          rel="noreferrer noopener"
-          style={{marginRight: "1rem"}}
-        >
-          Author
-        </a>
-        <a
           href="https://renchon.iamtakagi.vercel.app/privacy_policy"
           target="_blank"
           rel="noreferrer noopener"

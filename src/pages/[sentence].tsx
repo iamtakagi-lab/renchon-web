7157f5196cec7d2010f8799cd712498c27803a9f
takagi <iam.takagi.dev@gmail.com>
2022-10-18 06:21:05 +0900

    feat: セリフの生成回数をツイートに含める

--- src/pages/[sentence].tsx
+++ src/pages/[sentence].tsx
@@ -84,7 +84,7 @@ const SentencePage = ({ querySentence }: Props) => {
             window.open(
               `https://twitter.com/intent/tweet?text=単語を覚えるれんちょんbot` +
                 encodeURI(
-                  `「${sentence}」&url=${`https://renchon.iamtakagi.vercel.app/${encodeURIComponent(
+                  `「${sentence}」\r\n${count}回セリフを生成しました！&url=${`https://renchon.iamtakagi.vercel.app/${encodeURIComponent(
                     sentence
                   )}`}`
                 ),
